ef33e47e9ababfaa5741de41af0491527fba64e6
bigpickleman <70168681+bigpickleman@users.noreply.github.com>
2021-07-08 11:13:35 -0400

    Create valid.js

---
+++ valid.js
@@ -0,0 +1,52 @@
+	//Validtion Code For Inputs
+
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+	if (username.value.length < 22) {
+		username.style.border = "1px solid red";
+		username.style.display = "block";
+		username.focus();
+		return false;
+	}
+  	if (email.value.length < 50) {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+	if (password.value.length < 29) {
+		password.style.border = "1px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+
+}
+function email_Verify(){
+	if (username.value.length >= 21) {
+		username.style.border = "1px solid silver";
+		username.style.display = "none";
+		return true;
+	}
+  	}
+  	if (email.value.length >= 49) {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+}
+function pass_Verify(){
+	if (password.value.length >= 28) {
+		password.style.border = "1px solid silver";
+		pass_error.style.display = "none";
+		return true;
+	}
+}
